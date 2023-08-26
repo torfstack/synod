72e0b9070e67992445b71b939c0f766111fc3750
Max Torf <torfstack@gmail.com>
2023-08-26 11:56:20 +0200

    added tags

--- frontend/src/components/AddSecretModal.svelte
+++ frontend/src/components/AddSecretModal.svelte
@@ -1,20 +1,30 @@
 <script lang="ts">
     import {Secret} from "$lib/secret"
-    import {Button, Input, Label, Modal} from "flowbite-svelte";
+    import {Badge, Button, Input, Label, Modal, P} from "flowbite-svelte";
     import {slide} from "svelte/transition";
 
     export let uploadSecret: (n: Secret) => Promise<void>;
     export let openModal: boolean;
 
-    let inputKey: string, inputValue: string, inputUrl: string;
+    let inputKey: string, inputValue: string, inputUrl: string, inputTag: string, inputTags: string[] = []
 
     function handleSecret() {
         let secret = new Secret(
             inputKey,
             inputValue,
-            inputUrl
+            inputUrl,
+            inputTags
         );
         uploadSecret(secret)
+    }
+
+    function handleKeyPress(event: KeyboardEvent) {
+        console.log(event.key)
+        if (event.key == "Enter") {
+            inputTags.push(inputTag)
+            inputTag = ""
+            inputTags = inputTags
+        }
     }
 </script>
 
@@ -30,6 +40,19 @@
     <div class="mb-3">
         <Label>URL</Label>
         <Input bind:value={inputUrl} type="text"></Input>
+    </div>
+    <div class="mb-3">
+        <Label>Tags</Label>
+        <Input bind:value={inputTag} type="text" on:keypress={handleKeyPress}
+               placeholder="Type something and press 'Enter' to add a tag"></Input>
+        <P slot="left" class="p-2">
+            {#each inputTags as tag}
+                <Badge class="mr-2" transition={slide} dismissable>
+                    #{tag}
+                </Badge>
+            {/each}
+            &nbsp;
+        </P>
     </div>
     <svelte:fragment slot="footer">
         <Button color="alternative" data-bs-dismiss="modal">Close</Button>
--- frontend/src/lib/secret.ts
+++ frontend/src/lib/secret.ts
@@ -1,3 +1,8 @@
 export class Secret {
-    constructor(readonly key: string, readonly value: string, readonly url: string | undefined) {}
+    constructor(
+        readonly key: string,
+        readonly value: string,
+        readonly url: string | undefined,
+        readonly tags: string[] = []
+    ) {}
 }
