089e225f66feacccf53884053b41130b15cd98c5
Max Torf <torfstack@gmail.com>
2024-01-28 17:19:20 +0100

    changed backend url to kayvault.torfstack.de/api

--- frontend/src/lib/config.ts
+++ frontend/src/lib/config.ts
@@ -1,9 +1,9 @@
 const localhost = "http://localhost:8080";
 const ip = "http://192.168.178.52:8080"
-const deployed = "https://kapi.torfstack.de"
+const deployed = "https://kayvault.torfstack.de/api"
 
 const secretPath = "/secret"
 
-const backendUrl = ip
+const backendUrl = deployed
 const backendSecretsUrl = backendUrl + secretPath
 export default backendSecretsUrl
