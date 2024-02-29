1d14cade93daff069567a848796302066db075f7
Max Torf <torfstack@gmail.com>
2024-02-29 21:12:39 +0100

    changed url from .de to .com

--- frontend/src/lib/config.ts
+++ frontend/src/lib/config.ts
@@ -1,6 +1,6 @@
 const localhost = "http://localhost:8080";
 const ip = "http://192.168.178.52:8080"
-const deployed = "https://kayvault.torfstack.de/api"
+const deployed = "https://kayvault.torfstack.com/api"
 
 const secretPath = "/secret"
 
