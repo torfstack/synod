538a028c2f5791b5c405e60a27a5cb1903db5828
David Deininger <torfstack@gmail.com>
2025-09-19 22:31:40 +0200

    fix: made select secret full width

--- frontend/src/components/secrets/SecretsList.tsx
+++ frontend/src/components/secrets/SecretsList.tsx
@@ -18,7 +18,7 @@ export const SecretsList = (props: SecretListProps) => {
             <li className="list-row w-full" key={secret.id}>
                 <button className="btn btn-ghost w-full flex flex-col min-h-fit lg:text-lg p-1"
                         onClick={() => props.clickedSecret(secret)}>
-                    <div className="flex flex-col max-w-full items-start">
+                    <div className="flex flex-col w-screen max-w-full items-start">
                         <p className="text-semibold truncate max-w-full">{secret.key}</p>
                         <p className="font-normal italic truncate max-w-full">[{secret.url}]</p>
                         <div className="flex flex-row gap-2 max-w-full items-center truncate pt-1.5">
