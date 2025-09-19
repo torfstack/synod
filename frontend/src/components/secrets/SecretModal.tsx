20f337220dd494cfe83f83bb4fd8348c7e34ff9b
David Deininger <torfstack@gmail.com>
2025-09-19 23:07:31 +0200

    feat: add eye symbol to password input (#39)

--- frontend/src/components/secrets/SecretModal.tsx
+++ frontend/src/components/secrets/SecretModal.tsx
@@ -1,6 +1,7 @@
 import type {Secret} from "../../util/secret.ts";
 import {useEffect, useState} from "react";
 import {closeModal} from "../../util/modal.ts";
+import {Eye, EyeSlash} from "../../icons/Eye.tsx";
 
 type SecretModalProps = {
     handleSecret: (s: Secret) => Promise<void>
@@ -13,6 +14,7 @@ export const SecretModal = (props: SecretModalProps) => {
     const [url, setUrl] = useState(props.existingSecret?.url ?? "")
     const [tags, setTags] = useState<string[]>(props.existingSecret?.tags ?? [])
     const [tag, setTag] = useState("")
+    const [passwordVisible, setPasswordVisible] = useState(false)
 
     useEffect(() => {
         setName(props.existingSecret?.key ?? "")
@@ -51,10 +53,14 @@ export const SecretModal = (props: SecretModalProps) => {
         return () => setTags(prevState => prevState.filter(t => t !== tag));
     }
 
+    function togglePassword() {
+        const isPassword = document.getElementById("input-password")?.getAttribute("type") == "password"
+        setPasswordVisible(isPassword)
+    }
+
     return <dialog id="add_secret_modal" className="modal">
         <div className="modal-box">
             <form>
-
                 <fieldset className="fieldset">
                     <legend className="fieldset-legend">Add Secret</legend>
                     <div className="flex flex-col gap-4">
@@ -67,10 +73,14 @@ export const SecretModal = (props: SecretModalProps) => {
                         </label>
                         <label className="input">
                             Secret
-                            <input type="password" placeholder="*****" value={secret}
+                            <input id="input-password" type={passwordVisible ? "text" : "password"} placeholder="*****"
+                                   value={secret}
                                    onChange={(e) => setSecret(e.target.value)}
                                    className="grow validator" minLength={1} required title="Can not be empty"/>
                             <p id="secret-input-error" className="validator-hint">Can not be empty</p>
+                            <button type="button" onClick={togglePassword} className="btn btn-ghost btn-xs">
+                                {passwordVisible ? <Eye/> : <EyeSlash/>}
+                            </button>
                         </label>
                         <label className="input">
                             URL
