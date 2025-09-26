dbaefdff8e71d54d3f83f63ba5b84eeb13d831e2
David Deininger <torfstack@gmail.com>
2025-09-27 01:06:36 +0200

    WIP: hybrid encryption

--- backend/domain/service_crypto.go
+++ backend/domain/service_crypto.go
@@ -1,11 +1,15 @@
 package domain
 
 import (
+	"bytes"
 	"context"
+	"crypto/aes"
+	"crypto/cipher"
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/sha256"
 	"encoding/base64"
+	"encoding/binary"
 	"errors"
 	"slices"
 
@@ -13,6 +17,11 @@ import (
 )
 
 var _ CryptoService = &service{}
+
+var (
+	MarkerBytes    = []byte{0x64, 0x61, 0x74, 0x71}
+	RsaMarkerBytes = []byte{0x00, 0x00, 0x00, 0x01}
+)
 
 func (s *service) GenerateKeyPair() (models.KeyPair, error) {
 	priv, err := rsa.GenerateKey(rand.Reader, 2048)
@@ -26,12 +35,31 @@ func (s *service) GenerateKeyPair() (models.KeyPair, error) {
 	}, nil
 }
 
-func (s *service) EncryptSecret(_ context.Context, secret models.Secret, key rsa.PublicKey) (models.EncryptedSecret, error) {
-	encrypted, err := rsa.EncryptOAEP(sha256.New(), rand.Reader, &key, []byte(secret.Value), nil)
+func (s *service) EncryptSecret(
+	_ context.Context,
+	secret models.Secret,
+	key rsa.PublicKey,
+) (models.EncryptedSecret, error) {
+	aesKey, err := newAesKey()
 	if err != nil {
 		return models.EncryptedSecret{}, err
 	}
-	encrypted = append(append(MARKER_BYTES, RSA_MARKER_BYTES...), []byte(encrypted)...)
+	gcmCipher, err := aesCipher(aesKey)
+	if err != nil {
+		return models.EncryptedSecret{}, err
+	}
+	encryptedAesKey, err := rsa.EncryptOAEP(sha256.New(), rand.Reader, &key, aesKey, nil)
+	if err != nil {
+		return models.EncryptedSecret{}, err
+	}
+	l := intToBytes(len(encryptedAesKey))
+	ciphertext, nonce, err := encrypt([]byte(secret.Value), gcmCipher)
+	if err != nil {
+		return models.EncryptedSecret{}, err
+	}
+	nonceL := intToBytes(len(nonce))
+	cipherTextL := intToBytes(len(ciphertext))
+	encrypted := slices.Concat(MarkerBytes, RsaMarkerBytes, l, encryptedAesKey, nonceL, nonce, cipherTextL, ciphertext)
 	return models.EncryptedSecret{
 		ID:    secret.ID,
 		Value: base64.StdEncoding.EncodeToString(encrypted),
@@ -41,19 +69,39 @@ func (s *service) EncryptSecret(_ context.Context, secret models.Secret, key rsa
 	}, nil
 }
 
-func (s *service) DecryptSecret(_ context.Context, secret models.EncryptedSecret, key rsa.PrivateKey) (models.Secret, error) {
-	bytes, err := base64.StdEncoding.DecodeString(secret.Value)
+func (s *service) DecryptSecret(
+	_ context.Context,
+	secret models.EncryptedSecret,
+	key rsa.PrivateKey,
+) (models.Secret, error) {
+	b, err := base64.StdEncoding.DecodeString(secret.Value)
 	if err != nil {
 		return models.Secret{}, err
 	}
-	header, encryptedBytes := bytes[:8], bytes[8:]
-	if !slices.Equal(header, append(MARKER_BYTES, RSA_MARKER_BYTES...)) {
+	header := b[:8]
+	if !slices.Equal(header, slices.Concat(MarkerBytes, RsaMarkerBytes)) {
 		return models.Secret{}, errors.New("invalid encryption header bytes")
 	}
-	decrypted, err := rsa.DecryptOAEP(sha256.New(), rand.Reader, &key, encryptedBytes, nil)
+	encryptedAesKeyLength := bytesToInt(b[8:12])
+	encryptedAesKey := b[12 : 12+encryptedAesKeyLength]
+	aesKey, err := rsa.DecryptOAEP(sha256.New(), rand.Reader, &key, encryptedAesKey, nil)
 	if err != nil {
 		return models.Secret{}, err
 	}
+	nonceLength := bytesToInt(b[12+encryptedAesKeyLength : 12+encryptedAesKeyLength+4])
+	nonce := b[12+encryptedAesKeyLength+4 : 12+encryptedAesKeyLength+4+nonceLength]
+	cipherTextLength := bytesToInt(b[12+encryptedAesKeyLength+4+nonceLength : 12+encryptedAesKeyLength+4+nonceLength+4])
+	cipherText := b[12+encryptedAesKeyLength+4+nonceLength+4 : 12+encryptedAesKeyLength+4+nonceLength+4+cipherTextLength]
+
+	gcmCipher, err := aesCipher(aesKey)
+	if err != nil {
+		return models.Secret{}, err
+	}
+	decrypted, err := decrypt(cipherText, gcmCipher, nonce)
+	if err != nil {
+		return models.Secret{}, err
+	}
+
 	return models.Secret{
 		ID:    secret.ID,
 		Value: string(decrypted),
@@ -71,5 +119,46 @@ func (s *service) GetPrivateKey(ctx context.Context, userID int64) (rsa.PrivateK
 	return s.database.SelectPrivateKey(ctx, userID)
 }
 
-var MARKER_BYTES = []byte{0x64, 0x61, 0x74, 0x71}
-var RSA_MARKER_BYTES = []byte{0x00, 0x00, 0x00, 0x01}
+func newAesKey() ([]byte, error) {
+	keyBytes := make([]byte, 32)
+	read, err := rand.Reader.Read(keyBytes)
+	if err != nil {
+		return nil, err
+	}
+	if read != len(keyBytes) {
+		return nil, errors.New("failed to read random bytes")
+	}
+	return keyBytes, nil
+}
+
+func aesCipher(key []byte) (cipher.AEAD, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	return cipher.NewGCM(block)
+}
+
+func encrypt(plaintext []byte, cipher cipher.AEAD) (encrypted []byte, nonce []byte, err error) {
+	nonce = make([]byte, cipher.NonceSize())
+	if _, err = rand.Read(nonce); err != nil {
+		return
+	}
+	return cipher.Seal(nil, nonce, plaintext, nil), nonce, nil
+}
+
+func decrypt(ciphertext []byte, cipher cipher.AEAD, nonce []byte) ([]byte, error) {
+	return cipher.Open(nil, nonce, ciphertext, nil)
+}
+
+func intToBytes(length int) []byte {
+	buffer := new(bytes.Buffer)
+	// err is always nil
+	_ = binary.Write(buffer, binary.BigEndian, uint32(length))
+	b := buffer.Bytes()
+	return []byte{b[0], b[1], b[2], b[3]}
+}
+
+func bytesToInt(b []byte) int {
+	return int(binary.BigEndian.Uint32(b))
+}
